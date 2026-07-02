8575b21ec667095316a67d2264904cdfbf224972
KishiEdward <dzidanrh@gmail.com>
2026-07-02 20:00:24 +0700

    feat: initialize main app entry point

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/router/app_router.dart';
+import 'core/services/deeplink_service.dart';
+import 'core/theme/app_theme.dart';
+import 'core/utils/app_bloc_observer.dart';
+import 'injection/injection_container.dart' as di;
+
+late final DeeplinkService _deeplinkService;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Bloc.observer = const AppBlocObserver();
+
+  await Firebase.initializeApp();
+
+  await di.init();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+  ));
+
+  _deeplinkService = DeeplinkService(AppRouter.router);
+  await _deeplinkService.init();
+
+  runApp(const BookPayApp());
+}
+
+class BookPayApp extends StatelessWidget {
+  const BookPayApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'E-Money Book Store',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
+      routerConfig: AppRouter.router,
+    );
+  }
+}
